c4c6820f081b2ccf21240faba97d98c7328f771e
TomaszO <tomasz.ogorek@gmail.com>
2014-12-14 21:58:25 +0100

    v

--- Kurs4/test4.py
+++ Kurs4/test4.py
@@ -3,3 +3,29 @@ __author__ = 'test'
 
 
 
+
+def main():
+
+    name = input("Cześć jestem Zed motocyklista, jak masz na imię? ")
+    print("Miło Cię poznać " + name + "!")
+
+    choices = dict(
+        r6 = 'Yamaha, Super moja ulubiona marka',
+        yamaha = 'Yamaha, Super moja ulubiona marka',
+        gsxr= 'Suzuki, Miałem kiedys GS500F 2005r',
+        suzuki = 'Suzuki, Miałem kiedys GS500F 2005r',
+        cbr='Honda, podobno najlatwiejszy motocykl do jazdy',
+        honda='Honda, podobno najlatwiejszy motocykl do jazdy',
+        hp4='BMW 1000RR HP4, motocykl marzenie',
+        dukat='Ducatti paniagale, pozazdroscic',
+        Ducatti='Ducatti paniagale, pozazdroscic',
+    )
+    v = input('Jakim motocyklem jezdzisz?')
+    print(choices.get(v, "tego nie znam"))
+
+if __name__ == '__main__':
+    main()
+
+
+
+
