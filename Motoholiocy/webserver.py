a6811c2e5651920cd169c2c30dcf93b1131fc30e
TomaszO <tomasz.ogorek@gmail.com>
2015-02-11 22:11:34 +0100

    m

---
+++ Motoholiocy/webserver.py
@@ -0,0 +1,22 @@
+__author__ = 'test'
+
+import sys
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+Handler = SimpleHTTPRequestHandler
+Server = BaseHTTPServer.HTTPServer
+Protocol = "HTTP/1.0"
+
+if sys.argv[1:]:
+    port= int(sys.argv[1])
+else:
+    port = 8000
+
+server_address = ('127.0.0.1', port)
+
+Handler.protocol_version = Protocol
+httpd = Server(server_address, Handler)
+#sa = httpd.socket.getsockname()
+print("Servering HTTP")
+httpd.serve_forever()
